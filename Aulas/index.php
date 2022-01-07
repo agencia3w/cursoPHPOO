9fe7a7e601e40ffa326f8c89b1d3db7b86843083
Paulinho <paulinho@agencia3w.com.br>
2022-01-07 14:55:39 -0300

    Interface 1

--- Aulas/index.php
+++ Aulas/index.php
@@ -2,46 +2,20 @@
 
 require "vendor/autoload.php";
 
-class Head
+interface LoginInterface
 {
-    public function create()
+    public function login();
+}
+
+abstract class Login{
+    abstract public function logar();
+}
+
+class LoginUser implements LoginInterface{
+    public function login()
     {
-        return 'Criando a cabeça';
+
     }
 }
 
-class Hand
-{
-    public function create()
-    {
-        return 'Criando a mão';
-    }
-}
-
-class Person
-{
-
-    private $head;
-
-    public function __construct()
-    {
-        $this->head = new Head; //é essencial E DEPENDE da outra para a classe existir então composição
-    }
-
-    // quando a classe depende da outra MAS não é essencial para a outra existir, então usa-se agregação
-    public function create(Hand $hand)
-    {
-        $this->head->create();
-        $hand->create();
-    }
-}
-
-$person = new Person;
-echo $person->create(new Hand);
-
-/**
- * Quando usar herança, composição ou agregação
- * Heranca: a classe é um...?
- * Composição: a classe tem um ... e é essencial para existir
- * Agregação: a classe tem um ... e não é essencial para existir
- */
+$login = new LoginUser;
