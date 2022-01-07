e6bac53e9654f778e7afb9d09dedfb12295b8c54
Paulinho <paulinho@agencia3w.com.br>
2022-01-07 15:35:49 -0300

    Polymorphism 2

--- Aulas/index.php
+++ Aulas/index.php
@@ -2,35 +2,39 @@
 
 require "vendor/autoload.php";
 
-abstract class Banco
+interface EmailInterface
 {
-    public function depositar($valor)
-    {
-        return "depositando com juros de 0.3 o valor de {$valor}";
-    }
+    public function send();
 }
 
-class Itau extends Banco
+class Swift implements EmailInterface
 {
-
-    private $juros = 0.6;
-
-    public function depositar($valor)
-    {
-        return "depositando com juros de {$this->juros} o valor de {$valor}";
-    }
-}
-
-class Bradesco extends Banco
-{
-    private $juros = 1;
-
-    public function depositar($valor)
-    {
-        return "depositando com juros de {$this->juros} o valor de {$valor}";
+    public function send(){
+        return "enviando email com o swift";
     }
 }
 
 
-$itau = new Itau();
-echo $itau->depositar(100);
+class Mailer implements EmailInterface
+{
+    public function send()
+    {
+        return "enviando email com o Mailer";
+    }
+}
+
+class SendEmail{
+    private $email;
+
+    public function __construct(EmailInterface $email)
+    {
+        $this->email = $email;
+    }
+
+    public function send(){
+        return $this->email->send();
+    }
+}
+
+$email = new SendEmail(new Swift);
+echo $email->send();
