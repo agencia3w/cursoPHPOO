4b96a71586bf68a986cf016f2d59acda8356a45b
Paulinho <paulinho@agencia3w.com.br>
2022-01-07 16:27:34 -0300

    Exceptions

--- Aulas/index.php
+++ Aulas/index.php
@@ -2,13 +2,52 @@
 
 require "vendor/autoload.php";
 
-class ShoppingCart
+
+class Controller
 {
-    public function __toString()
+    public function make()
     {
-        return "Chamando o toString";
+        if (!$this->controllerExist()) {
+            throw new ControllerNotExistsException;
+        }
+
+        if (!$this->methodExist()) {
+            throw new MethodNotExistsException;
+        }
+
+        return 'controller';
+    }
+
+    private function controllerExist()
+    {
+        return true;
+    }
+
+    private function methodExist()
+    {
+        return false;
     }
 }
 
-$shoppingCart = new ShoppingCart;
-echo $shoppingCart;
+class ControllerNotExistsException extends Exception
+{
+    public function message()
+    {
+        return "Erro ao criar o controller";
+    }
+}
+
+class MethodNotExistsException extends Exception
+{
+    public function message()
+    {
+        return "Método não existe";
+    }
+}
+
+try {
+    $controller = new Controller;
+    $controller->make();
+} catch (Exception $e) {
+    var_dump($e->message());
+}
