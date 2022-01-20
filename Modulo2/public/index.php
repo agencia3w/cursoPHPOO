47329e8ce4de5e59aab040461ff097a84ae55197
Paulinho <paulinho@agencia3w.com.br>
2022-01-20 09:53:25 -0300

    Interface 1

--- Modulo2/public/index.php
+++ Modulo2/public/index.php
@@ -2,21 +2,38 @@
 
 declare(strict_types=1);
 
-class Teste
+interface CartInterface
 {
-    public function t(): string
+    public function add($product): void;
+    public function remove($product): void;
+    public function quantity($quantity, $product): void;
+    public function clear(): void;
+    public function cart(): array;
+}
+
+class CartShop implements CartInterface
+{
+    public function add($product): void
     {
-        return 'teste na classe pai';
+    }
+
+    public function remove($product): void
+    {
+    }
+
+    public function quantity($quantity, $product): void
+    {
+    }
+
+    public function clear(): void
+    {
+    }
+
+    public function cart(): array
+    {
+        return [];
     }
 }
 
-class TesteSub extends Teste
-{
-    public function t(): string
-    {
-        return 'teste na classe filha';
-    }
-}
-
-$teste = new TesteSub();
-echo $teste->t();
+$cart = new CartShop;
+$cart->add('monitor');
