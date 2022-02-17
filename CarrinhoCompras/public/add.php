a24a31cbc4d0fa9c4bcfb41248bd8e445ff2734e
Paulinho <paulinho@agencia3w.com.br>
2022-02-17 09:41:05 -0300

    Add product to cart

--- CarrinhoCompras/public/add.php
+++ CarrinhoCompras/public/add.php
@@ -1,4 +1,13 @@
 <?php
 
+session_start();
+require '../vendor/autoload.php';
+
+use app\classes\Cart;
+
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-echo $id;
+
+$cart = new Cart;
+$cart->add($id);
+
+header('Location: /');
