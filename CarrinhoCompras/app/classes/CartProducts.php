2d53849eb5e201235c2d2476d90f7ece9d3d88d3
Paulinho <paulinho@agencia3w.com.br>
2022-02-17 15:07:06 -0300

    Read from Model

--- CarrinhoCompras/app/classes/CartProducts.php
+++ CarrinhoCompras/app/classes/CartProducts.php
@@ -2,6 +2,7 @@
 
 namespace app\classes;
 
+use app\database\models\Read;
 use app\interfaces\CartInterface;
 
 class CartProducts
@@ -9,21 +10,24 @@ class CartProducts
 	public function products(CartInterface $cartInterface)
 	{
 		$productsInCart = $cartInterface->cart();
-		$productsInDatabase = require '../app/helpers/products.php';
+		// $productsInDatabase = require '../app/helpers/products.php';
+		$productsInDatabase = (new Read)->all('products');
 
 		$products = [];
 		$total = 0;
 
 		foreach ($productsInCart as $productId => $quantity) {
-			$product = $productsInDatabase[$productId];
+			$product = [...array_filter($productsInDatabase, fn ($product) => (int)$product->id === $productId)];
+
+			// $product = $productsInDatabase[$productId];
 			$products[] = [
 				'id' => $productId,
-				'product' => $product['name'],
-				'price' => $product['price'],
+				'product' => $product[0]->name,
+				'price' => $product[0]->price,
 				'qty' => $quantity,
-				'subtotal' => $quantity * $product['price'],
+				'subtotal' => $quantity * $product[0]->price,
 			];
-			$total += $quantity * $product['price'];
+			$total += $quantity * $product[0]->price;
 		}
 
 		return [
