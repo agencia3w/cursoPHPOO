3833ce6be1d2758c6da2a6ab6ae073df7d7f27f3
Paulinho <paulinho@agencia3w.com.br>
2022-03-04 12:15:59 -0300

    Dynamic Params

--- MVC/public/index.php
+++ MVC/public/index.php
@@ -4,17 +4,23 @@ require "../bootstrap.php";
 
 use core\Controller;
 use core\Method;
+use core\Parameters;
 
 // dd(app\classes\Uri::uri());
 
 try {
     $controller = new Controller;
-    $controller->load();
+    $controller = $controller->load();
     // dd($controller);
 
     $method = new Method;
     $method = $method->load($controller);
-    $controller->$method();
+
+    $parameters = new Parameters;
+    $parameters = $parameters->load();
+
+    $controller->$method($parameters);
+
 
 } catch (\Exception $e) {
     dd($e->getMessage());
